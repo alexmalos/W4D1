0ec2595611792a70c6941fa103bbd725bfae4131
Alex Malos <malosalexander@gmail.com>
2022-06-13 16:14:09 -0400

    cleaned up poly tree node in knights travails

--- knights_travails/poly_tree_node.rb
+++ knights_travails/poly_tree_node.rb
@@ -15,37 +15,7 @@ class PolyTreeNode
         @parent.children << self  unless @parent==nil
     end
 
-    # def inspect
-    #     @children
-    # end
-
     def add_child(child_node)
         child_node.parent=self
-    end
-
-    def remove_child(child_node)
-        raise "this is a parent" if child_node.parent == nil
-        child_node.parent = nil
-    end
-
-    def dfs(target_value)
-        return self if @value == target_value
-        @children.each do |child|
-            child_dfs = child.dfs(target_value)
-            return child_dfs unless child_dfs == nil
-        end
-        nil
-    end
-
-    def bfs(target_value)
-        arr=[self]
-        while arr.length !=0
-            if arr[0].value == target_value
-                return arr[0]
-            else
-                arr+=arr[0].children
-                arr.shift         
-            end
-        end
     end
 end
